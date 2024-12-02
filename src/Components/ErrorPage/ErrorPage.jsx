f2a703ab3ce8d516e80c7cacfa63379750cc6cac
T0nci <138928016+T0nci@users.noreply.github.com>
2024-12-02 14:14:48 +0100

    Create and style an error page

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,5 +1,23 @@
+import { Link, useRouteError } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
+
 const ErrorPage = () => {
-  return <></>;
+  const error = useRouteError();
+  console.error(error);
+
+  let errorMessage = null;
+  if (error.status) errorMessage = `${error.status}: ${error.statusText}`;
+  else if (error.error) errorMessage = `${error.error}`;
+  else errorMessage = `${error}`;
+
+  return (
+    <div className={styles.parent}>
+      <p className={styles.error}>{errorMessage}</p>
+      <Link to="/" className={styles.link}>
+        Go back to the homepage
+      </Link>
+    </div>
+  );
 };
 
 export default ErrorPage;
