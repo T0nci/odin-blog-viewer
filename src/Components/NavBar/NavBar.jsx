8145fa12dc7c09d8190dac82a77052873bf5052f
T0nci <138928016+T0nci@users.noreply.github.com>
2024-12-02 14:15:43 +0100

    Create and style the NavBar

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,49 @@
+import { Outlet, Link } from "react-router-dom";
+import { useState } from "react";
+import styles from "./NavBar.module.css";
+import "../../global.css";
+
 const NavBar = () => {
-  return <></>;
+  const [token, setToken] = useState(localStorage.getItem("token"));
+
+  return (
+    <>
+      <nav>
+        <ul className={`${styles.nav} container`}>
+          <li className={styles.home}>
+            <Link to="/" className={styles["nav-link"]}>
+              webdev.blog
+            </Link>
+          </li>
+          {token ? (
+            <li>
+              <Link to="/logout" className={styles["nav-link"]}>
+                Logout
+              </Link>
+            </li>
+          ) : (
+            <>
+              <li>
+                <Link to="/login" className={styles["nav-link"]}>
+                  Login
+                </Link>
+              </li>
+              <li>
+                <Link to="/register" className={styles["nav-link"]}>
+                  Register
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+      <main>
+        <div className="container">
+          <Outlet context={{ setToken }} />
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default NavBar;
