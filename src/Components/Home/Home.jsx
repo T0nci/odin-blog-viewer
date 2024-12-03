984c6d5269ef4f08bf2cc4c65a03032c5566f31c
T0nci <138928016+T0nci@users.noreply.github.com>
2024-12-03 16:54:21 +0100

    Create and style the homepage

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,77 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { formatDate, formatContent } from "../../utils";
+import styles from "./Home.module.css";
+
 const Home = () => {
-  return <>Home</>;
+  const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let isActive = true;
+
+    fetch(import.meta.env.VITE_API_URL + "/posts")
+      .then((res) => res.json())
+      .then((response) => {
+        if (isActive) {
+          setPosts(
+            response.posts.map((post) => {
+              return { ...post, date_published: new Date(post.date_published) };
+            }),
+          );
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        setPosts(err);
+      });
+
+    return () => (isActive = false);
+  }, []);
+
+  return (
+    <>
+      <h1 className={styles.heading}>Welcome to my blog!</h1>
+      <p className={styles.paragraph}>
+        Hello, my name is T0nci, and I am a dedicated full-stack web developer
+        with a passion for continuous learning and growth within the
+        ever-evolving tech industry. Through my work, I strive to provide
+        valuable insights and share effective strategies that can help aspiring
+        developers enhance their skills. My goal with this blog is to equip you
+        with the knowledge, tools, and exclusive techniques that will guide you
+        toward mastering the art of web development. Whether you&apos;re just
+        starting your journey or looking to refine your expertise, you will find
+        a new piece of knowledge or a useful tip with every step you take here.
+      </p>
+      {posts instanceof Error ? (
+        <p className={styles.error}>Error loading posts</p>
+      ) : posts.length > 0 ? (
+        <ul className={styles.cards}>
+          {posts.map((post) => (
+            <li
+              key={post.id}
+              onClick={() => navigate(`/posts/${post.id}`)}
+              className={styles.card}
+            >
+              <p className={styles.title}>{post.title}</p>
+              <p className={styles.content}>{formatContent(post.content)}</p>
+              <p className={styles.date}>{formatDate(post.date_published)}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className={styles.loading}>
+          <div className={styles.box}>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+            <div className={styles.dot}></div>
+          </div>
+          Loading posts
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Home;
