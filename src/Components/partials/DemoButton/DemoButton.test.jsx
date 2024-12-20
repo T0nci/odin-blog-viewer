48ee8df14640c0d5853ce56f8cd932a64c162074
T0nci <138928016+T0nci@users.noreply.github.com>
2024-12-21 00:06:12 +0100

    Write unit tests for DemoButton

---
+++ src/Components/partials/DemoButton/DemoButton.test.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable no-undef */
+import { describe, it, expect, vi } from "vitest";
+import { screen, render, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import DemoButton from "./DemoButton";
+
+global.fetch = vi.fn();
+const navigate = vi.fn();
+const setDisplayName = vi.fn();
+
+describe("DemoButton Component", () => {
+  it("renders", () => {
+    let container = null;
+    act(() => {
+      const { container: temp } = render(
+        <DemoButton
+          navigate={() => {}}
+          setErrors={() => {}}
+          setDisplayName={() => {}}
+        />,
+      );
+      container = temp;
+    });
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <button>
+          Try Demo Account
+        </button>
+      </div>
+    `);
+  });
+
+  it("deletes post", async () => {
+    const setItem = vi.fn();
+    global.localStorage = { setItem };
+    const json = vi.fn().mockResolvedValueOnce({
+      token: "some token value",
+      displayName: "Tester",
+    });
+    global.fetch.mockResolvedValueOnce({ json });
+
+    act(() =>
+      render(
+        <DemoButton
+          navigate={navigate}
+          setErrors={() => {}}
+          setDisplayName={setDisplayName}
+        />,
+      ),
+    );
+    const user = userEvent.setup();
+
+    await act(async () => await user.click(screen.getByRole("button")));
+
+    expect(navigate).toHaveBeenCalledWith("/");
+    expect(setDisplayName).toHaveBeenCalledWith("Tester");
+    expect(setItem).toHaveBeenCalledWith("token", "some token value");
+    expect(setItem).toHaveBeenCalledWith("displayName", "Tester");
+  });
+});
